11f014394a7cd5b0c1820756d5aa03e80f361c75
Abdullah-irfan <83572170+Abdullah-irfan@users.noreply.github.com>
2021-05-19 19:08:58 +0530

    generic, delegates
    generic, delegates

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace csharp_generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            generics<string> check = new generics<string>();
+            bool res = check.compar("a", "a");
+            Console.WriteLine(res);
+
+            values obj1 = new values(dela.add);
+            int res1 = obj1(30, 400);
+            Console.WriteLine("result{0}\n", res1);
+
+            values obj2 = new values(dela.sub);
+            int res2 = obj2(100,50);
+            Console.WriteLine("result{0}\n", res2);
+
+           
+
+
+
+            Console.ReadKey();
+        }
+    }
+    public  class generics<T>
+    {
+        public  bool compar(T a, T b)
+        {
+           if(a.Equals(b))
+            {
+                return true;
+            }
+           else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ dela.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp_generics
+{
+    public delegate int values(int a, int b);
+    class dela
+    {
+        public static int add(int a, int b)
+        {
+            return (a + b);
+        }
+
+        public static int sub(int a, int b)
+        {
+            return (a - b);
+        }
+    }
+    
+}
